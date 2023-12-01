f54ffee82c8df535961387fc3f35eb33f7ef2250
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-12-01 22:36:29 +0700

    TV:

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,12 @@ import { useSelector } from 'react-redux';
 import classes from './Home.module.scss';
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className={classes.home}>
+            <input className={classes.home_input} placeholder="nhap" />
+            <p className={classes.home_text}>huynh vinh</p>
+        </div>
+    );
 };
 
 export default Home;
