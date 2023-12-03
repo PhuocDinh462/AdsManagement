d4277ab585d62c881465cfdbb08cc384b6996241
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-12-03 16:20:42 +0700

    TV:

--- src/components/sidebar/sideBar.js
+++ src/components/sidebar/sideBar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ItemSide from './ItemSide/ItemSide';
+import classes from './SideBar.module.scss';
 
-const SideBar = () => {
-    return <div>SideBar</div>;
+const SideBar = ({ data }) => {
+    const [selectedItemId, setSelectedItemId] = useState(1);
+
+    const handleItemClick = (id) => {
+        setSelectedItemId(id);
+    };
+    return (
+        <div className={classes.sidebar}>
+            <p className={classes.sidebar__title}>Quản lý</p>
+            {data.map((item, index) => (
+                <ItemSide
+                    key={index}
+                    item={item}
+                    isSelected={selectedItemId === item.id}
+                    onItemClick={handleItemClick}
+                />
+            ))}
+        </div>
+    );
 };
 
 export default SideBar;
