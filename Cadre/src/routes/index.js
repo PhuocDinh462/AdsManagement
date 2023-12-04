c3ce230a20d1c63bf445a215160a7cb044169e3a
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-12-04 15:52:20 +0700

    TT: User Profile

--- Cadre/src/routes/index.js
+++ Cadre/src/routes/index.js
@@ -5,6 +5,7 @@ import Home from '../pages/home/Home';
 import NotFound from '../pages/notFound';
 import Layout from './../layouts/index';
 import ManageDistrictWard from '../pages/ManageDistrictWard/ManageDistrictWard';
+import Infor from '../pages/Infor/Infor';
 
 const Navigation = () => {
     const authenticated = true;
@@ -13,6 +14,9 @@ const Navigation = () => {
             <Routes>
                 <Route element={<Layout />}>
                     <Route path="/" name="home" element={<Home />} />
+                </Route>
+                <Route element={<Layout />}>
+                    <Route path="/infor" name="infor" element={<Infor />} />
                 </Route>
                 <Route path="*" name="notFound" element={<Navigate to="/" />} />
             </Routes>
