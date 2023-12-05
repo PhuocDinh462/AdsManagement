6ab2304c7610ccfcab4d628b6cf20b1b5458e020
Huỳnh Tấn Vinh <huynhtanvinhktqn123@gmail.com>
2023-12-05 17:14:02 +0700

    TV: set up map

--- Civilian/src/pages/home/Home.js
+++ Civilian/src/pages/home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import classes from './Home.module.scss';
-import SideBar from '../../components/sideBar/SideBar';
 
 const Home = () => {
     return (
         <div className={classes.home}>
             <div style={{ height: '60px', width: '100%', background: 'blue' }}>NavBar</div>
-            <SideBar />
         </div>
     );
 };
