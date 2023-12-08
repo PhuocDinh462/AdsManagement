3e166be2c758ea46da6fc225f0befbb8625ae973
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2023-12-08 16:03:17 +0700

    TT: Add state input field in infor page

--- Cadre/src/pages/Infor/components/Form/index.js
+++ Cadre/src/pages/Infor/components/Form/index.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import classes from './Form.module.scss';
 
+const userInfor = {
+  phone: "0388888888",
+  dob: '2002-04-30',
+  email: 'something@gmail.com',
+  name: 'Nguyen Van A',
+  address: '123 Dương Thị Mười',
+}
 const Form = () => {
   // Khởi tạo trạng thái cho các thông tin
+  const inputPhone = useRef();
+  const inputDob = useRef();
+  const inputEmail = useRef();
+  const inputFullName = useRef();
+  const inputAddress = useRef();
+
   const [phone, setPhone] = useState('');
   const [dob, setDob] = useState('');
   const [email, setEmail] = useState('');
   const [fullName, setFullName] = useState('');
   const [address, setAddress] = useState('');
+
+  const [editState, setEditState] = useState(false);
 
   // Hàm xử lý khi giá trị thay đổi
   const handleChange = (e) => {
@@ -34,41 +49,69 @@ const Form = () => {
         break;
     }
   };
+  const handleEditState = () => {
+
+    setEditState(false)
+  }
+  useEffect(() => {
+    inputPhone.current.value = userInfor.phone
+    inputDob.current.value = userInfor.dob
+    inputEmail.current.value = userInfor.email
+    inputFullName.current.value = userInfor.name
+    inputAddress.current.value = userInfor.address
+
+    setPhone(userInfor.phone)
+    setDob(userInfor.dob)
+    setEmail(userInfor.email)
+    setFullName(userInfor.name)
+    setAddress(userInfor.address)
+  }, [editState])
 
   return (
     <div>
       <div className={classes['first-row']}>
         <label className={classes['title-input']}>
           Điện thoại liên lạc:
-          <input type="tel" name="phone" value={phone} onChange={handleChange} />
+          <input type="tel" name="phone" readOnly={!editState} ref={inputPhone} value={phone} onChange={handleChange} />
         </label>
 
         <label className={classes['title-input']}>
           Ngày sinh:
-          <input type="date" name="dob" value={dob} onChange={handleChange} />
+          <input type="date" name="dob" readOnly={!editState} ref={inputDob} value={dob} onChange={handleChange} />
         </label>
       </div>
 
       <div className={classes['second-row']}>
         <label className={classes['title-input']}>
           Email:
-          <input type="email" name="email" value={email} onChange={handleChange} />
+          <input type="email" name="email" readOnly={!editState} ref={inputEmail} value={email} onChange={handleChange} />
         </label>
       </div>
 
       <div className={classes['third-row']}>
         <label className={classes['title-input']}>
           Họ và Tên:
-          <input type="text" name="fullName" value={fullName} onChange={handleChange} />
+          <input type="text" name="fullName" readOnly={!editState} ref={inputFullName} value={fullName} onChange={handleChange} />
         </label>
       </div>
 
       <div className={classes['fourth-row']}>
         <label className={classes['title-input']}>
           Địa chỉ:
-          <input type="text" name="address" value={address} onChange={handleChange} />
+          <input type="text" name="address" readOnly={!editState} ref={inputAddress} value={address} onChange={handleChange} />
         </label>
       </div>
+      {editState ? (
+        <div className={classes['wrapper-handle']}>
+          <button className={classes['cancel-button']} type="button" onClick={() => setEditState(false)}>Huỷ</button>
+          <button className={classes['update-button']} type="button" onClick={handleEditState}>Cập nhật</button>
+
+        </div>
+
+      ) : (
+        <button className={classes['custom-button']} type="button" onClick={() => setEditState(true)}>Chỉnh sửa</button>
+      )}
+
     </div>
   );
 };
