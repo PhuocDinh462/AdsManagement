4660e38f11bcbe6adab687b923b4e7dc3e89b4ac
Phuoc Dinh <phuocdinh462@gmail.com>
2023-12-17 21:17:39 +0700

    HP: Update navbar

--- Cadre/src/pages/WardAndDistrict/Home/index.js
+++ Cadre/src/pages/WardAndDistrict/Home/index.js
@@ -1,17 +1,59 @@
 import classes from './styles.module.scss';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import FilterDropdown from '~components/Dropdown/FilterDropdown';
 import GoongAutoComplete from '~components/GoongAutoComplete';
 import SpotInfoSidebar from '~components/SpotInfoSidebar';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+};
+
+const center = {
+  lat: 10.763781,
+  lng: 106.684918,
+};
 
 export default function Home() {
   const [filterActive, setFilterActive] = useState(false);
+  const [sidebarActive, setSidebarActive] = useState(false);
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_APIKEY,
+    language: 'vi',
+    region: 'vn',
+  });
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
 
   return (
     <div className={classes.main_container}>
-      <h1 className={classes.map_container}>Map</h1>
+      <div className={classes.map_container}>
+        {isLoaded ? (
+          <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10} onLoad={onLoad} onUnmount={onUnmount}>
+            {/* Child components, such as markers, info windows, etc. */}
+            <></>
+          </GoogleMap>
+        ) : (
+          <>Loading...</>
+        )}
+      </div>
 
       <div className={classes.filter}>
         {filterActive && <FilterDropdown />}
@@ -27,7 +69,7 @@ export default function Home() {
         <GoongAutoComplete apiKey={process.env.REACT_APP_GOONG_APIKEY} placeholder="Tìm kiếm theo địa chỉ" />
       </div>
 
-      <SpotInfoSidebar />
+      {sidebarActive && <SpotInfoSidebar />}
     </div>
   );
 }
