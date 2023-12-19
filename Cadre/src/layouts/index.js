57a14395b2ce14ea1acb915e30a43edf0a266213
Phuoc Dinh <phuocdinh462@gmail.com>
2023-12-20 00:39:29 +0700

    HP: Fix bug sidebar, fix navbar

--- Cadre/src/layouts/index.js
+++ Cadre/src/layouts/index.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 import { Outlet } from 'react-router-dom';
 import NavBar from '~components/navBar';
 import SideBars from '../components/sidebar/SideBars';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+
+const navbarCategories = [
+  // {
+  //   name: 'Đăng ký',
+  //   icon: faKey,
+  //   path: '/create-account',
+  // },
+];
 
 const Layout = () => {
   return (
     <div className="">
-      <NavBar hideCategories />
+      <NavBar categories={navbarCategories} />
       <div style={{ display: 'flex' }}>
         <SideBars />
         <Outlet />
