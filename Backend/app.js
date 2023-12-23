4a0e8f825e07a575a5425632ef18c8b4b404f7c1
Bao <20127557@student.hcmus.edu.vn>
2023-12-23 14:35:49 +0700

    BL: add socket

--- Backend/app.js
+++ Backend/app.js
@@ -10,11 +10,13 @@ const googleRoute = require('./routes/googleRoute.js');
 const wardRoute = require('./routes/wardRoute.js');
 
 const app = express();
+
 dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT;
 
 app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use('/', googleRoute);
@@ -23,7 +25,25 @@ app.use('/account', authenticateUser, accountRoute);
 app.use('/cadre', cadreRoute);
 app.use('/ward', wardRoute);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server app listening on port ${port}`);
 });
 
+const socketIo = require('socket.io')(server, {
+  cors: { origin: '*' },
+});
+
+//const io = require('./app'); // ví dụ require ở file khác để sử dụng
+
+socketIo.on('connection', (socket) => {
+  ///Handle khi có connect từ client tới
+  console.log('New client connected' + socket.id);
+
+  socket.emit('update', socket.id); // Example send event to client
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected'); // Khi client disconnect thì log ra terminal.
+  });
+});
+
+module.exports = socketIo;
